d59eed79e3f59eede72793a02529f8f818efac44
rodrigodenner <rodrigodenner.art@gmail.com>
2024-08-27 09:21:07 -0300

    fix: update OrderController.php

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -21,4 +21,35 @@ class OrderController extends Controller
     $order = Order::create($request->all());
     return response()->json(new OrderResource($order), 201);
   }
+
+  public function show($id)
+  {
+    $orders = Order::with(['customer', 'bread', 'meat', 'optional'])->find($id)->first();
+    if (!$orders) {
+      return response()->json(['message' => 'Pedido não encontrado'], 404);
+    }
+    return response()->json(new OrderResource($orders), 200);
+  }
+
+  public function update(Request $request, $id)
+  {
+    $order = Order::find($id);
+    if (!$order) {
+      return response()->json(['message' => 'Pedido não encontrado'], 404);
+    }
+
+    $order->update($request->all());
+    return new OrderResource($order);
+  }
+
+  public function destroy($id)
+  {
+    $order = Order::find($id);
+    if (!$order) {
+      return response()->json(['message' => 'Pedido não encontrado'], 404);
+    }
+
+    $order->delete();
+    return response()->json(['message' => 'Pedido deletado com sucesso'], 200);
+  }
 }
